334c795a12d0b77d9730e2a111eb8cea0bc95ac3
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2016-05-20 16:37:49 -0400

    Updated statistics script.

--- get_statistics.sh
+++ get_statistics.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+DATE=$(date +'%Y%m%d')
+OUTPUT_FILENAME=$DATE.csv
 wget --quiet --output-document temp.txt http://stream.wrct.org:8000/status.xsl 
 STAT=`grep "Listen" temp.txt | cut -d":" -f9 | cut -d">" -f3 | cut -d"<" -f1`
-echo  $(date +'%m/%d/%Y')","$(date +'%R')","$STAT >> statistics.csv
+echo $(date '+%A')","$(date +'%m/%d/%Y')","$(date +'%R')","$STAT >> $OUTPUT_FILENAME
 rm temp.txt
